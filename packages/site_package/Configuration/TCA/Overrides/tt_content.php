6d802255c12d05772e7fabd82786ab113ac31fb7
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2024-06-05 19:59:51 +0200

    working on custom content element

--- packages/site_package/Configuration/TCA/Overrides/tt_content.php
+++ packages/site_package/Configuration/TCA/Overrides/tt_content.php
@@ -27,12 +27,22 @@ defined('TYPO3') or die();
 $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['site_package_newcontentelement'] = 'content-text';
 
 // Configure the default backend fields for the content element
+
+// --div--; --> Registers a new Tab 
+//  LLL:EXT:....; --> The title of the Tab which in this case it is placed under the Language folder of your extension
+// The third part defines which fields are going to be displayed and in which sequence.
+// --palettes--; --> Inserts the palette you have previously defined
+// LLL:EXT:....;  --> The title of the Palette which in this case it is placed under the Language folder of your extension
+// The name of your palette
 $GLOBALS['TCA']['tt_content']['types']['site_package_newcontentelement'] = [
     'showitem' => '
             --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
                --palette--;;general,
-               header; Internal title (not displayed),
+               --palette--;;headers,
                bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                --palette--;;frames,
+                --palette--;;appearanceLinks,
             --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
                --palette--;;hidden,
                --palette--;;access,
